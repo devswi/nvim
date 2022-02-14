22c447f7aa6e319e556fd09101635c161cf2f825
shiwei93 <stayfocusjs@gmail.com>
2022-02-14 20:38:28 +0800

    feat: add gx command

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,5 +8,21 @@ map('n', '<TAB>', ':BufferLineCycleNext<CR>')
 map('n', '<S-TAB>', ':BufferLineCyclePrev<CR>')
 map('n', '<C-x>', ':BufferLinePickClose<CR>')
 
--- leader
---
+-- gx mapping
+function _G.open_in_browser()
+  local Logger = require('utils.logger')
+  local url = string.match(vim.fn.expand('<cWORD>'), '[https|http]+://[^ >"\',;]*')
+  if url ~= nil then
+    if vim.fn.has('mac') == 1 then
+      vim.cmd(('!open %s'):format(url))
+    elseif vim.fn.has('unix') == 1 then
+      vim.cmd(('!xdg-open %s'):format(url))
+    else
+      Logger:error('gx is not supoorted on this OS!')
+    end
+  else
+    Logger:error('No https or http URI found in line.')
+  end
+end
+
+map('n', 'gx', '<cmd>lua open_in_browser()<cr>')
