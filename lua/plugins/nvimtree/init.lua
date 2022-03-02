ef4f4ff0e3da5053a02db4a5294e4ee21eef502b
shiwei93 <stayfocusjs@gmail.com>
2022-03-02 16:02:57 +0800

    fix: nvim_tree options

--- lua/plugins/nvimtree/init.lua
+++ lua/plugins/nvimtree/init.lua
@@ -6,16 +6,10 @@ g.nvim_tree_add_trailing = 0
 g.nvim_tree_git_hl = 0
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_root_folder_modifier = table.concat({ ':t:gs?$?/..', string.rep(' ', 1000), '?:gs?^??' })
 g.nvim_tree_refresh_wait = 300
 
 g.nvim_tree_special_files = {}
-
-g.nvim_tree_window_picker_exclude = {
-  filetype = { 'notify', 'packer', 'qf' },
-  buftype = { 'terminal' },
-}
 
 g.nvim_tree_icons = {
   default = '',
@@ -67,5 +61,16 @@ require('nvim-tree').setup({
   },
   diagnostics = {
     enable = false,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = false,
+      window_picker = {
+        exclude = {
+          filetype = { 'notify', 'packer', 'qf' },
+          buftype = { 'terminal' },
+        },
+      },
+    },
   },
 })
